f4b20f77c83c36e35047d51ac0428af9f65ed444
Roy Ivy III <RIVY.dev@gMail.com>
2012-08-12 22:01:44 -0500

    UPDATE: version update
    * using Pandora JSON API (via Pithos project library @ https://github.com/kevinmehall/pithos)

--- default.py
+++ default.py
@@ -6,7 +6,7 @@ import os, sys
 __title__ = "Pandora"
 __script_id__ = "script.xbmc.pandora"
 __settings__ = xbmcaddon.Addon(id=__script_id__)
-__version__ = "1.2.10-git"
+__version__ = "2.1.0"
 
 print "PANDORA: Initializing v%s" %__version__
 print "PANDORA: sys.platform = %s" %sys.platform
